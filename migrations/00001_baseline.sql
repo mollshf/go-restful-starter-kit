09901ce7a273334497c34ab49d376ffc6a6723cc
maulana <kikymaulana00@gmail.com>
2026-04-19 22:58:13 +0700

    Refactor database schema: rename and restructure tables for user roles and permissions. Update foreign key constraints and add missing fields for better data integrity and management.

--- migrations/00001_baseline.sql
+++ migrations/00001_baseline.sql
@@ -1,58 +1,12 @@
 -- +goose Up
 CREATE SCHEMA IF NOT EXISTS "public";
 
-CREATE TABLE "public"."permissions" (
-    "id" uuid NOT NULL DEFAULT gen_random_uuid(),
-    "permission_name" varchar NOT NULL,
-    "module" varchar NOT NULL,
-    "act" varchar NOT NULL,
-    "created_at" timestamp DEFAULT CURRENT_TIMESTAMP,
-    "deleted_at" timestamp,
-    PRIMARY KEY ("id")
-);
-
 CREATE TABLE "public"."user_roles" (
     "id" uuid NOT NULL DEFAULT gen_random_uuid(),
     "role_id" uuid NOT NULL,
     "user_id" uuid NOT NULL,
     "created_at" timestamp DEFAULT CURRENT_TIMESTAMP,
     "deleted_at" timestamp,
-    PRIMARY KEY ("id")
-);
-
-CREATE TABLE "public"."accounts" (
-    "id" uuid NOT NULL DEFAULT gen_random_uuid(),
-    "user_id" uuid NOT NULL,
-    "password_hash" varchar NOT NULL,
-    "provider" varchar,
-    "provider_account_id" varchar,
-    "created_at" timestamp DEFAULT CURRENT_TIMESTAMP,
-    "updated_at" timestamp,
-    PRIMARY KEY ("id")
-);
-
-CREATE TABLE "public"."users" (
-    "id" uuid NOT NULL DEFAULT gen_random_uuid(),
-    "fullname" varchar NOT NULL,
-    "username" varchar NOT NULL,
-    "email" varchar NOT NULL,
-    "verified_email" boolean NOT NULL DEFAULT false,
-    "created_at" timestamp DEFAULT CURRENT_TIMESTAMP,
-    "updated_at" timestamp,
-    "deleted_at" timestamp,
-    PRIMARY KEY ("id")
-);
-
-CREATE TABLE "public"."sessions" (
-    "id" uuid NOT NULL DEFAULT gen_random_uuid(),
-    "user_id" uuid NOT NULL,
-    "active_role" uuid,
-    "token_hash" varchar NOT NULL,
-    "csrf_token" varchar NOT NULL,
-    "ip_address" varchar NOT NULL,
-    "user_agent" varchar NOT NULL,
-    "created_at" timestamp DEFAULT CURRENT_TIMESTAMP,
-    "expired_at" timestamp,
     PRIMARY KEY ("id")
 );
 
@@ -66,11 +20,56 @@ CREATE TABLE "public"."roles" (
     PRIMARY KEY ("id")
 );
 
+CREATE TABLE "public"."accounts" (
+    "id" uuid NOT NULL DEFAULT gen_random_uuid(),
+    "user_id" uuid NOT NULL,
+    "password_hash" varchar NOT NULL,
+    "provider" varchar,
+    "provider_account_id" varchar,
+    "created_at" bigint DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" bigint,
+    PRIMARY KEY ("id")
+);
+
+CREATE TABLE "public"."user_sessions" (
+    "id" uuid NOT NULL DEFAULT gen_random_uuid(),
+    "user_id" uuid NOT NULL,
+    "token_hash" varchar NOT NULL,
+    "ip_address" varchar NOT NULL,
+    "user_agent" varchar NOT NULL,
+    "active_role" uuid,
+    "created_at" timestamp DEFAULT CURRENT_TIMESTAMP,
+    "expired_at" timestamp,
+    PRIMARY KEY ("id")
+);
+
 CREATE TABLE "public"."role_permissions" (
     "id" uuid NOT NULL DEFAULT gen_random_uuid(),
     "permission_id" uuid NOT NULL,
     "role_id" uuid NOT NULL,
     "created_at" timestamp DEFAULT CURRENT_TIMESTAMP,
+    "deleted_at" timestamp,
+    PRIMARY KEY ("id")
+);
+
+CREATE TABLE "public"."permissions" (
+    "id" uuid NOT NULL DEFAULT gen_random_uuid(),
+    "permission_name" varchar NOT NULL,
+    "module" varchar NOT NULL,
+    "act" varchar NOT NULL,
+    "created_at" timestamp DEFAULT CURRENT_TIMESTAMP,
+    "deleted_at" timestamp,
+    PRIMARY KEY ("id")
+);
+
+CREATE TABLE "public"."users" (
+    "id" uuid NOT NULL DEFAULT gen_random_uuid(),
+    "fullname" varchar NOT NULL,
+    "username" varchar NOT NULL,
+    "email" varchar NOT NULL,
+    "verified_email" boolean NOT NULL DEFAULT false,
+    "created_at" timestamp DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" timestamp,
     "deleted_at" timestamp,
     PRIMARY KEY ("id")
 );
@@ -88,12 +87,13 @@ CREATE TABLE "public"."user_permissions" (
 -- Schema: public
 ALTER TABLE "public"."role_permissions" ADD CONSTRAINT "fk_role_permissions_permission_id_permissions_id" FOREIGN KEY("permission_id") REFERENCES "public"."permissions"("id");
 ALTER TABLE "public"."user_permissions" ADD CONSTRAINT "fk_user_permissions_permission_id_permissions_id" FOREIGN KEY("permission_id") REFERENCES "public"."permissions"("id");
-ALTER TABLE "public"."user_roles" ADD CONSTRAINT "fk_user_roles_role_id_roles_id" FOREIGN KEY("role_id") REFERENCES "public"."roles"("id");
 ALTER TABLE "public"."role_permissions" ADD CONSTRAINT "fk_role_permissions_role_id_roles_id" FOREIGN KEY("role_id") REFERENCES "public"."roles"("id");
+ALTER TABLE "public"."user_sessions" ADD CONSTRAINT "fk_user_sessions_active_role_roles_id" FOREIGN KEY("active_role") REFERENCES "public"."roles"("id");
+ALTER TABLE "public"."user_roles" ADD CONSTRAINT "fk_user_roles_role_id_roles_id" FOREIGN KEY("role_id") REFERENCES "public"."roles"("id");
 ALTER TABLE "public"."accounts" ADD CONSTRAINT "fk_accounts_user_id_users_id" FOREIGN KEY("user_id") REFERENCES "public"."users"("id");
-ALTER TABLE "public"."user_roles" ADD CONSTRAINT "fk_user_roles_user_id_users_id" FOREIGN KEY("user_id") REFERENCES "public"."users"("id");
 ALTER TABLE "public"."user_permissions" ADD CONSTRAINT "fk_user_permissions_user_id_users_id" FOREIGN KEY("user_id") REFERENCES "public"."users"("id");
-ALTER TABLE "public"."sessions" ADD CONSTRAINT "fk_sessions_user_id_users_id" FOREIGN KEY("user_id") REFERENCES "public"."users"("id");
+ALTER TABLE "public"."user_roles" ADD CONSTRAINT "fk_user_roles_user_id_users_id" FOREIGN KEY("user_id") REFERENCES "public"."users"("id");
+ALTER TABLE "public"."user_sessions" ADD CONSTRAINT "fk_user_sessions_user_id_users_id" FOREIGN KEY("user_id") REFERENCES "public"."users"("id") ON DELETE CASCADE;
 -- +goose Down
 DROP TABLE IF EXISTS "public"."user_permissions" CASCADE;
 DROP TABLE IF EXISTS "public"."role_permissions" CASCADE;
